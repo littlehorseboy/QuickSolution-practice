ec49ce73c2b7fb813b795fbb3b33a76d4faba255
littlehorseboy <littlehorseboy@gmail.com>
2020-02-23 22:20:59 +0800

    加入專案檔案。

---
+++ Calendar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuickDate
+{
+	internal class Calendar
+	{
+        static void Main(string[] args)
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"今天的日期是 {now}");
+            Console.ReadLine();
+        }
+
+        internal static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+	}
+}
+
---
+++ QuickDate/Calendar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("QuickTest")]
+
+namespace QuickDate
+{
+    internal class Calendar
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"現在的時間是 {now}");
+            Console.ReadLine();
+        }
+
+        internal static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
